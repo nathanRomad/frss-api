221e82b594023e09e6055b79393ef8716c8df5a7
nathanRomad <nathan.a.hamilton@gmail.com>
2021-06-16 16:25:55 -0500

    build: summaryView to send specific analysis back to clieint

--- frssapi/models/answers.py
+++ frssapi/models/answers.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 from .questions import Questions
 from .options import Options
 
+
 class Answers(models.Model):
     input_answer = models.IntegerField(null=True)
     option_id = models.ForeignKey(Options, null=True, on_delete=models.CASCADE)
     question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     class Meta:
         unique_together = ("question_id", "user_id")
