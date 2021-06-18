495db4d6f91aa663c43165ae8270591fa687b02d
nathanRomad <nathan.a.hamilton@gmail.com>
2021-06-18 14:05:50 -0500

    deploy: adjusting model charfield to textfield

--- frssapi/models/questions.py
+++ frssapi/models/questions.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Questions(models.Model):
     text = models.CharField(max_length=255)
-    explanation = models.CharField(max_length=255, null=True)
+    explanation = models.TextField(null=True)
     type = models.CharField(max_length=255)
